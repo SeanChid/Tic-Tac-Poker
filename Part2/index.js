5ac2cf3aced405963b7466dfd88c5a2c2837ca33
SeanChid <seanchidester737@gmail.com>
2023-11-09 16:39:46 -0800

    Third Commit

---
+++ Part2/index.js
@@ -0,0 +1,7 @@
+const idInput = document.querySelector('#idInput')
+const colorInput = document.querySelector('#colorInput')
+
+const setCard = (element) => {
+    const card = document.querySelector(`#${idInput.value}`)
+
+}
